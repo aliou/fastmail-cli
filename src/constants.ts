75917d6fa88815ee8a4488a32f96538d67a8c688
aliou <980367+aliou@users.noreply.github.com>
2026-01-23 17:03:34 +0000

    release: v0.1.0

--- src/constants.ts
+++ src/constants.ts
@@ -1,5 +1,5 @@
 export const APP_NAME = "fastmail";
-export const VERSION = "0.0.1";
+export const VERSION = "0.1.0";
 
 export const JMAP_SESSION_URL = "https://api.fastmail.com/jmap/session";
 export const CONFIG_FILE = "config.json";
